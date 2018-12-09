6a7a1d39985c4ca941f8a05b76b3117795575efc
Jason Qu <fishinsea2014@gmail.com>
2018-12-09 20:19:18 +1300

    Do a test for Spring IOC and DI

---
+++ JQ.OA/SpringNet/Program.cs
@@ -0,0 +1,22 @@
+﻿using Spring.Context;
+using Spring.Context.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpringNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test for spring .net");
+            IApplicationContext ctx = ContextRegistry.GetContext();
+            IUserInfoService lister = (IUserInfoService)ctx.GetObject("UserInfoService");
+            Console.WriteLine(lister.ShowMsg());
+            Console.Read();
+        }
+    }
+}
---
+++ JQ.OA/SpringNet/UserInfoservice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpringNet
+{
+    public class UserInfoservice : IUserInfoService
+    {
+        public string UserName { get; set; }
+        public Person Person { get; set; }
+
+        public string ShowMsg()
+        {
+            return "Hello :" + UserName + "and the age is : " + Person.Age;
+        }
+    }
+}
