12c58922bb0916c2a044da8d60928c594532e944
Jason Qu <fishinsea2014@gmail.com>
2018-12-10 14:09:44 +1300

    Complete log funciton.

--- JQ.OA/JQ.OA.WebApp/Global.asax.cs
+++ JQ.OA/JQ.OA.WebApp/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using Spring.Web.Mvc;
+﻿using JQ.OA.WebApp.Models;
+using Spring.Web.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -16,9 +19,30 @@ namespace JQ.OA.WebApp
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);//Here register myException 
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Write the errors into log file
+            string fileLogPath = Server.MapPath("/Log/");
+            //Get a thread from thread pool to monitor the exceptions
+            ThreadPool.QueueUserWorkItem((waitCallBack) =>  //The parameter is a lambda function
+            { 
+                while (true)
+                {
+                    if (MyExceptionAttribute.ExceptionQueue.Count > 0)
+                    {
+                        Exception ex = MyExceptionAttribute.ExceptionQueue.Dequeue(); //Dequeue an item
+                        string fileName = DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+                        File.AppendAllText(fileLogPath + fileName, ex.ToString(), System.Text.Encoding.Default);
+                    }
+                    else
+                    {
+                        Thread.Sleep(3000);
+                    }
+                }
+            }, fileLogPath);
+
         }
     }
 }
