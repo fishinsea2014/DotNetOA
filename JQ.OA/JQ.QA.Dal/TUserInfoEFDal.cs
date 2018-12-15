20ce99be151da0850c3dca20b1f54eef0f57990a
Jason Qu <fishinsea2014@gmail.com>
2018-12-15 15:40:19 +1300

    This is a test branch for refactor the database model

---
+++ JQ.OA/JQ.QA.Dal/TUserInfoEFDal.cs
@@ -0,0 +1,17 @@
+﻿using JQ.QA.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JQ.QA.Dal
+{
+    public class TUserInfoEFDal
+    {
+        public  int AddUserInfo(UserInfo userInfo)
+        {
+            return 1;
+        }
+    }
+}
