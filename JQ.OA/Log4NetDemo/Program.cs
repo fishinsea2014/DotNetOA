95fc967c4dead2884c1184dee521e859f6bfffe9
Jason Qu <fishinsea2014@gmail.com>
2018-12-16 00:51:41 +1300

    Log4net demo

---
+++ JQ.OA/Log4NetDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            log4net.Config.XmlConfigurator.Configure();
+            ILog log = log4net.LogManager.GetLogger("hello");
+            log.Debug("shitle");
+            log.Fatal("50 ...");
+            int a = 4, b = 0;
+            int c = a / b;
+
+        }
+    }
+}
